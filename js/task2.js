f5f416cc3609268c54471f6817c2e62e03f8052c
Kopychyn <kopychyni@gmail.com>
2023-12-10 19:03:33 +0200

    add use strict

--- js/task2.js
+++ js/task2.js
@@ -1,4 +1,5 @@
-function getShippingMessage (country, price, deliveryFee) {
+'use strict';
+function getShippingMessage(country, price, deliveryFee) {
 	const totalPrice = price + deliveryFee;
 	return `Shipping to ${country} will cost ${totalPrice} credits`;
 };
